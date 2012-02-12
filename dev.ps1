55307f0048e48e6fb1a6966741bc1662c623bd4a
Alec Lanter <kintar1900@gmail.com>
2012-02-12 10:37:41 -0600

    Updated dev script

--- dev.ps1
+++ dev.ps1
@@ -1,23 +1,14 @@
 param($category, $project)
 
 $devbase = "D:\Development"
-$dotnetBase = $devBase + "\dotnet"
-$eftBase = $dotnetBase + "\eft"
-$legacyBase = $dotnetBase + "\eft-legacy"
-$gitBase = $devbase + "\git"
 $rubyBase = $devbase + "\ruby"
 $pythonBase = $devbase + "\python"
 
-$categories = @{"dotnet" = $dotnetBase
-               ;"cao" = ($dotNetBase + "\CardAtOnce")
-               ;"eft" = $eftBase
-               ;"eftlib" = ($eftBase + "\Libraries")
-               ;"eftapp" = ($eftBase + "\Applications")
-               ;"eftsvc" = ($eftBase + "\Services")
-               ;"legacy" = $legacyBase
-               ;"git" = $gitBase
-               ;"ruby" = $rubyBase
-               ;"python" = $pythonBase}
+$categories = @{"main" = $devBase
+               ;"emarker" = $devBase + "\eMarker"
+			   ;"ruby" = $rubyBase
+			   ;"python" = $pythonBase
+			   ;"powershell" = $devBase + "\powershell"}
                
 if (($category -eq $null) -or (($categories.Contains($category) -eq $false)))
 {

80652843a6650692e592fdbac00ab1f42724b52e
Alec Lanter <kintar1900@gmail.com>
2013-03-25 08:09:53 -0500

    Added recurse option to rm command.

--- sandblast.ps1
+++ sandblast.ps1
@@ -6,7 +6,7 @@ $char = $result.KeyChar
 if  (($char -eq 'y') -or ($char -eq 'Y'))
 {
 	write-host -ForegroundColor green "`n*click* *whirrrrrrr*  *FWOOOSH!*"
-	gci -Recurse | Where-Object { $_.FullName -match ".*\\bin\\.*" } | ForEach-Object { rm $_.fullname }
+	gci -Recurse | Where-Object { $_.FullName -match ".*\\bin\\.*" } | ForEach-Object { rm -r $_.fullname }
 	write-host -ForegroundColor green "Aaaaah, that felt good."
 }
 else
